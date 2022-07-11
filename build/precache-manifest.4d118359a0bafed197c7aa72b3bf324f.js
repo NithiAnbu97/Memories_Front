e50ee89627603774dd29fecb79492e7193d760a4
Nithiyanantham S A <nithianbu97@gmail.com>
2022-07-11 18:48:55 +0530

    initial commits

---
+++ build/precache-manifest.4d118359a0bafed197c7aa72b3bf324f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1f58820a09851c937eb69c7a476617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0729b4ab87acb14314f3",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "5941e8e0a12d771fd81e",
+    "url": "/static/js/2.84a66d0e.chunk.js"
+  },
+  {
+    "revision": "5a1079c98d77e0c0ee6fa744686de0de",
+    "url": "/static/js/2.84a66d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0729b4ab87acb14314f3",
+    "url": "/static/js/main.a2328151.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "7cd5204b526499464998ab6c1424e078",
+    "url": "/static/media/memories.7cd5204b.png"
+  }
+]);
